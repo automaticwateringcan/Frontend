7fb417301bc17d0854ff0ebb20f62822c2023def
Bartosz Gaj <bartosz.gaj4@gmail.com>
2019-04-21 23:02:01 +0200

    Added custom fonts

--- android/app/src/main/java/com/wateringcanfront/MainApplication.java
+++ android/app/src/main/java/com/wateringcanfront/MainApplication.java
@@ -3,6 +3,7 @@ package com.wateringcanfront;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import cl.json.RNSharePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSharePackage(),
             new RNGestureHandlerPackage()
       );
     }
--- src/components/FirstScreen.js
+++ src/components/FirstScreen.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import {Text, TouchableNativeFeedback, View} from "react-native";
+import {Image, ScrollView, Text, TouchableNativeFeedback, View, StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+    image: {
+        width: 320, height: 100, resizeMode: 'contain'
+    },
+    text: {
+fontFamily: 'Poppins-ExtraLight',
+fontWeight: '200',
+fontSize: 20,
+color: 'rgba(83,113,75,1)',
+    }
+});
 
 class FirstScreen extends React.Component {
+
+
+
     render() {
         return (
 
             <TouchableNativeFeedback onPress={() => this.props.navigation.navigate('Login')}>
                 <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                    <Text>WITAMY</Text>
-                    <Text>touch anywhere</Text>
+                    <Image style={styles.image}
+                        source={require('../../resources/logo_text_A0_Rectangle_2_pattern.png')}
+                   />
+                    <Text style={styles.text}> we take care of them</Text>
                 </View>
             </TouchableNativeFeedback>
         );
